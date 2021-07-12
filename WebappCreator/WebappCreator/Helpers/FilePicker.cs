ba1452903c035efc62dc38076a2b8c6c22309d43
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2021-07-12 23:23:20 +0200

    Add files via upload
    First commit: added file and folder picker

---
+++ WebappCreator/WebappCreator/Helpers/FilePicker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebappCreator.Helpers
+{
+    public class FilePicker
+    {
+        public static string OpenFileDialog()
+        {
+            string filename = null;
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                filename = dlg.FileName;
+            }
+            return filename;
+            
+        }
+    }
+}
